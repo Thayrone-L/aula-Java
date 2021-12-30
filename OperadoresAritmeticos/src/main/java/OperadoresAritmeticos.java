d2ea2fe97500e307ede7e6229bef1bc5dcb13107
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2021-12-30 10:17:03 -0300

    inserção de instruções sobre incremento e decremento

--- OperadoresAritmeticos/src/main/java/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/main/java/OperadoresAritmeticos.java
@@ -14,10 +14,38 @@ public class OperadoresAritmeticos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        /*
         int n1=3;
         int n2=5;
         float media=(n1+n2)/2;
-        System.out.printf("A media entre %s e %s é %.2f",n1,n2,media);
+        System.out.printf("A media entre %s e %s é %.2f",n1,n2,media);*/
+        //pré incremento
+        int  numero=5;
+        int valor = 5 + ++numero;
+        System.out.println("Pre incremento");
+        System.out.println("a variável valor vale: "+valor);
+        System.out.println("a variável numero vale: "+numero);
+        //pós incremento
+        int  numero2=5;
+        int valor2 = 5 + numero2++;
+        System.out.println("Pós incremento");
+        System.out.println("a variável valor 2 vale: "+valor2);
+        System.out.println("a variável numero 2 vale: "+numero2);
+        
+        /* O mesmo será válido para o decremento */
+        
+         int  numero3=5;
+        int valor3 = 5 + --numero3;
+        System.out.println("Pre decremento");
+        System.out.println("a variável valor vale: "+valor3);
+        System.out.println("a variável numero vale: "+numero3);
+        //pós incremento
+        int  numero4=5;
+        int valor4 = 5 + numero4--;
+        System.out.println("Pos decremento");
+        System.out.println("a variável valor 2 vale: "+valor4);
+        System.out.println("a variável numero 2 vale: "+numero4);
     }
     
 }
