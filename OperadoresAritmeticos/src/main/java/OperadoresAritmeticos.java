a4377b755c36250a951db3ff7b3d83890990c99f
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2021-12-30 09:49:11 -0300

    Aula 7
    Exercícios da aula 7, divisor inteiro, usando try/catch para lançar as devidas mensagens de erro

---
+++ OperadoresAritmeticos/src/main/java/OperadoresAritmeticos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author thayrone
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1=3;
+        int n2=5;
+        float media=(n1+n2)/2;
+        System.out.printf("A media entre %s e %s é %.2f",n1,n2,media);
+    }
+    
+}
