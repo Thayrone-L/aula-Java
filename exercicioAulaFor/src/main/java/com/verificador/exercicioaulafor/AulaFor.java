2e6ea8d6fe95083ba71aee0801d7636590ed43ae
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2022-01-11 10:58:31 -0300

    aula 13, exercicio sobre for

---
+++ exercicioAulaFor/src/main/java/com/verificador/exercicioaulafor/AulaFor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.verificador.exercicioaulafor;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author thayrone
+ */
+public class AulaFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=0;
+        for(int cc=1; cc<=3; cc++){
+        
+            n+= Integer.parseInt(JOptionPane.showInputDialog(null,"Informe a" + cc +"º nota"));
+        
+        }
+        float media = n/3;
+        JOptionPane.showMessageDialog(null,"A media é:" + media);
+    }
+    
+}
