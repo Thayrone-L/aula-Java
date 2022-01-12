deba10e6e6a0440d6d87fa896d5077511a26762e
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2022-01-12 09:26:36 -0300

    final do modulo basico, inicio a orientação a objetos

--- ExercicioAula15/src/exercicioaula15/ExercicioAula15.java
+++ ExercicioAula15/src/exercicioaula15/ExercicioAula15.java
@@ -2,24 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package funcao01;
+package exercicioaula15;
 
 /**
  *
  * @author thayrone
  */
-public class Funcao01 {
+public class ExercicioAula15 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    
-    Fatorial f = new Fatorial();
-    f.setValor(5);
-        System.out.print(f.getFormula());
-        System.out.print(f.getFatorial());
     }
     
 }
---
+++ ExercicioAula15/src/exercicioaula15/Fatorial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicioaula15;
+
+/**
+ *
+ * @author thayrone
+ */
+public class Fatorial {
+    
+    private int num=0;
+    private int fat=1;
+    private String formula="";
+    
+}
